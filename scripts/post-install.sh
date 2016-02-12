138ca5cce90410fc222d4736cb0b15ed2c902793
Kevin Gautreau <contact@kgaut.net>
2016-02-12 14:04:14 +0100

    change www to web

--- scripts/post-install.sh
+++ scripts/post-install.sh
@@ -1,32 +1,32 @@
 #!/bin/sh
 
 # Prepare the settings file for installation
-if [ ! -f www/sites/default/settings.php ]
+if [ ! -f web/sites/default/settings.php ]
   then
-    cp www/sites/default/default.settings.php www/sites/default/settings.php
-    chmod 777 www/sites/default/settings.php
+    cp web/sites/default/default.settings.php web/sites/default/settings.php
+    chmod 777 web/sites/default/settings.php
     echo "default.settings.php copied to settings.php"
 fi
 
 # Prepare the services file for installation
-if [ ! -f www/sites/default/services.yml ]
+if [ ! -f web/sites/default/services.yml ]
   then
-    cp www/sites/default/default.services.yml www/sites/default/services.yml
-    chmod 777 www/sites/default/services.yml
+    cp web/sites/default/default.services.yml web/sites/default/services.yml
+    chmod 777 web/sites/default/services.yml
     echo "default.services.yml copied to services.yml"
 fi
 
 # Prepare the files directory for installation
-if [ ! -d www/sites/default/files ]
+if [ ! -d web/sites/default/files ]
   then
-    mkdir -m777 www/sites/default/files
-    touch www/sites/default/files/.gitkeep
+    mkdir -m777 web/sites/default/files
+    touch web/sites/default/files/.gitkeep
     echo "files folder created"
 fi
 
-if [ ! -d www/sites/default/tmp ]
+if [ ! -d web/sites/default/tmp ]
   then
-    mkdir -m777 www/sites/default/tmp
-    touch www/sites/default/tmp/.gitkeep
+    mkdir -m777 web/sites/default/tmp
+    touch web/sites/default/tmp/.gitkeep
     echo "tmp folder created"
 fi
